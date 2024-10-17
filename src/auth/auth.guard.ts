2f27de0a952c50c8480b2789d2d454bf6b5a58c6
pig19980 <pig19980@kaist.ac.kr>
2024-10-18 01:06:42 +0900

    websocket용 guard 추가

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -3,15 +3,25 @@ import { Request } from 'express';
 import { Response } from 'express';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthHttpGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
+    console.log('1=====================');
     const request: Request = context.switchToHttp().getRequest();
     const response: Response = context.switchToHttp().getResponse();
     console.log(request.cookies);
     response.user = '123';
-    console.log('1=====================');
     console.log(response);
     console.log(response.user);
+    return true;
+  }
+}
+
+@Injectable()
+export class AuthWsGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    console.log('2=====================');
+    const data = context.switchToWs().getData();
+    console.log(data);
     return true;
   }
 }
